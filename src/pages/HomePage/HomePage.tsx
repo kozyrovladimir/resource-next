1705a9280f6a7147e2fa4fb1c81f19d7112fe607
Vladimir Kozyro <45574893+kozyrovladimir@users.noreply.github.com>
2024-08-08 20:49:22 +0300

    feature: validation search params

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,8 +4,11 @@ import React from 'react';
 import {SearchPanel} from "@/features/SearchPanel";
 import {ViewSwitch} from "@/features/ViewSwitch";
 import VideoList from "@/widgets/VideoList/VideoList";
+import {useValidateSearchParams} from "@/utils/hooks/useValidateSearchParams";
 
 const HomePage = () => {
+  useValidateSearchParams();
+
   const [checked, setChecked] = React.useState(false);
 
   return (
