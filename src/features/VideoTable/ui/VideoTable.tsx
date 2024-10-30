35e3ccaae9a9fe15174ebdd077172681b6b4e4ff
Vladimir Kozyro <45574893+kozyrovladimir@users.noreply.github.com>
2024-10-30 11:17:06 +0300

    fix: link to flow page

--- src/features/VideoTable/ui/VideoTable.tsx
+++ src/features/VideoTable/ui/VideoTable.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Paper,
   Table, TableBody,
   TableCell,
   TableContainer,
@@ -15,8 +14,9 @@ import {
 } from "@/shared/lib/helpers";
 import {useUpdateQueryString} from "@/utils/hooks/useUpdateQueryString";
 import styles from './VideoTable.module.scss';
+import Link from "next/link";
 
-const TableRowItem: React.FC<{video: VideoListItemI}> = ({video}) => {
+const TableRowItem: React.FC<{ video: VideoListItemI }> = ({video}) => {
 
   const phaseColor = definePhaseColor(video.phase);
   const elementColor = defineElementColor(video.element);
@@ -44,19 +44,25 @@ const TableRowItem: React.FC<{video: VideoListItemI}> = ({video}) => {
   return (
     <TableRow>
       <TableCell sx={{maxWidth: '420px'}}>
+        <Link href={`/video/${video.id}`} style={{color: '#222'}} className={styles.tableText}>
         {video.title}
+          </Link>
       </TableCell>
       <TableCell sx={{color: phaseColor}} align="left">
-          <span className={styles.tableText} onClick={phaseOnClick}>{video.phase && formatTagString(video.phase)}</span>
+        <span className={styles.tableText}
+              onClick={phaseOnClick}>{video.phase && formatTagString(video.phase)}</span>
       </TableCell>
       <TableCell sx={{color: seasonColor}} align="left">
-          <span className={styles.tableText} onClick={seasonOnClick}>{video.season && formatTagString(video.season)}</span>
+        <span className={styles.tableText}
+              onClick={seasonOnClick}>{video.season && formatTagString(video.season)}</span>
       </TableCell>
       <TableCell sx={{color: elementColor}} align="left">
-          <span className={styles.tableText} onClick={elementOnClick}>{video.element && formatTagString(video.element)}</span>
+        <span className={styles.tableText}
+              onClick={elementOnClick}>{video.element && formatTagString(video.element)}</span>
       </TableCell>
       <TableCell sx={{color: organColor}} align="left">
-          <span className={styles.tableText} onClick={organOnClick}>{video.organ && formatTagString(video.organ)}</span>
+        <span className={styles.tableText}
+              onClick={organOnClick}>{video.organ && formatTagString(video.organ)}</span>
       </TableCell>
     </TableRow>
   );
@@ -72,16 +78,17 @@ const VideoTable: React.FC<VideoTableType> = ({videoList}) => {
       <Table sx={{minWidth: 650}}>
         <TableHead sx={{backgroundColor: 'var(--color-main-grey-light)'}}>
           <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="left">Phase</TableCell>
-            <TableCell align="left">Season</TableCell>
-            <TableCell align="left">Element</TableCell>
-            <TableCell align="left">Organ</TableCell>
+            <TableCell sx={{color: '#666'}}
+                       className={styles.tableHeadText}>Name</TableCell>
+            <TableCell sx={{color: '#666'}} align="left">Phase</TableCell>
+            <TableCell sx={{color: '#666'}} align="left">Season</TableCell>
+            <TableCell sx={{color: '#666'}} align="left">Element</TableCell>
+            <TableCell sx={{color: '#666'}} align="left">Organ</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {videoList.map((video) => (
-            <TableRowItem video={video} />
+            <TableRowItem video={video}/>
           ))}
         </TableBody>
       </Table>
