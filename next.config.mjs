b6e819a920d0bb87e76e9bd43152b933dedf1e82
Vladimir Kozyro <45574893+kozyrovladimir@users.noreply.github.com>
2024-10-30 10:40:27 +0300

    fix: proxy settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const API_URL = process.env.API_PATH;
+
 const nextConfig = {
     images: {
         minimumCacheTTL: 60,
@@ -17,6 +19,14 @@ const nextConfig = {
     },
     experimental: {
         missingSuspenseWithCSRBailout: false,
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${API_URL}/:path*`,
+            },
+        ]
     },
 };
 
